3c8e82ac74b5fbec63d1461dfd1e80d952ba5cc7
BrewBarred <elayjahh7@hotmail.com>
2026-04-30 10:47:23 +1200

    Quick back-up before attempting to fix minor bug with testUpdateById and UpdateById in test/model.js classes

--- test.js
+++ test.js
@@ -6,6 +6,14 @@
 // import the database connection and model
 const db = require("./app/models/db");
 const Book = require("./app/models/books.model");
+// define the properties for a book object
+const testBook = new Book({
+    title: "The tales of the test tickle title",
+    author: "Test, The Third",
+    genre: "Genre Shree",
+    price: "9.69",
+    stock: "420"
+});
 
 // track the id of the book we create so we can cross-reference it between tests
 let bookId;
@@ -13,9 +21,9 @@ let bookId;
 // helper function to handle logging neatly
 const log = (test, err, result) => {
     if (err) {
-        console.log(`Test failed! Error occurred while [${test}]:`, err);
+        console.log(`Test failed! Error occurred while ${test}:`, err);
     } else {
-        console.log(`Test passed! Error occurred while [${test}]`, result);
+        console.log(`Test passed! No errors occured while ${test}`, result);
     }
 };
 
@@ -25,24 +33,12 @@ const log = (test, err, result) => {
 const testCreateANewBook = () => {
     console.log("\n--- Test 1: Create a new book ---");
 
-    // define a test book to insert
-    const testBook = new Book({
-        title: "The tales of the test tickle title",
-        author: "Test, The Third",
-        genre: "Genre Shree",
-        price: "9.69",
-        stock: "420"
-    });
-
-    ///
-    /// TEST 1: Create a new book
-    ///
     Book.create(testBook, (err, result) => {
         console.log("\n---Test 1: Create a new book", err, result);
 
         // update the class test book for cross-referencing between test functions
         bookId = result.id;
-        console.log(`Created book with ID: ${bookId}`);
+        console.log(`creating a new book with ID: ${bookId}`);
         // call get all books to chain test functions together
         testGetAll();
     });
@@ -55,7 +51,7 @@ const testGetAll = () => {
     console.log("\n---Test 2: Get all books from the database ---");
 
     Book.getAll((err, result) => {
-        log("Get all books in the database", err, `Found ${result ? result.length : 0} books`);
+        log("fetching all books in the database", err, `Found ${result ? result.length : 0} books`);
         // call next test to keep chaining
         testFindById();
     });
@@ -68,15 +64,48 @@ const testFindById = () => {
     console.log("\n--- Test 3: Find book by ID ---");
 
     Book.findById(bookId, (err, result) => {
-        log("finding by its ID", err, result);
+        log("finding a book by its bookID", err, result);
         // call next test to keep chaining
-        // TODO
+        testUpdateById();
     });
 };
+
+///
+/// Test 4: Update a book by ID
+///
+const testUpdateById = (id, book, result) => {
+    console.log("\n--- Test 4: Update a book by ID ---");
+
+    // create a query to update a book
+    db.query(
+        // request to update
+        "UPDATE books SET title=?, author=?, genre=?, price=?, stock=? WHERE id=?",
+        // define the new values (taken from the passed book object)
+        [book.title, book.author, book.genre, book.price, book.stock, id],
+        // process the response
+        (err, res) => {
+            // if the response represents an error
+            if (err) {
+                // print error and return early
+                result(err, null);
+                return;
+            }
+            
+            // else if the response says we updated 0 rows, we were unsuccessful
+            if (res.affectedRows==0) {
+                // return informative error message before returning
+                result({ kind: "not_found" }, null);
+                return;
+            }         
+            
+            // else if we get here the update was successful, use the spread operator (...book) to unpack book properties
+            result(null, { id: id, ...book });
+        }
+    )
+}
 
 // TODO: next test
 
 
-
 // start test-chain when this class is called
-createANewBook();
+testCreateANewBook();
