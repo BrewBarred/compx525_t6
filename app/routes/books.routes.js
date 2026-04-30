c1037e10d768d36e38c743637e2d0e644bd9740a
BrewBarred <elayjahh7@hotmail.com>
2026-04-30 15:08:54 +1200

    Halway through building app module exports

--- app/routes/books.routes.js
+++ app/routes/books.routes.js
@@ -1,6 +1,17 @@
 module.exports = app => {
-  const projects = require("../controllers/books.controller");
+  // import the books controller so we can call its functions
+  const books = require("../controllers/books.controller");
 
   // TODO: complete the code as per the instructions given README.md
-  
+  module.exports = app => {
+    // add a new books object to the database
+    app.post("/books", books.create);
+
+    // retrieve every book from the database
+    app.get("/books", books.findAll);
+
+    // get books by name
+    app.get("/books/title/:title", books.findByName);
+
+  };
 }
